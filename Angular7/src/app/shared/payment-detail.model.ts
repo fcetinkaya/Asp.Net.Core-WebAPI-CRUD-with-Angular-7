77a95538d7c771659bf8a705bb81efac6e515357
fatih.cetinkaya <fatih.cetinkaya@FTH>
2019-03-13 10:22:48 +0300

    Initial commit

---
+++ Angular7/src/app/shared/payment-detail.model.ts
@@ -0,0 +1,7 @@
+export class PaymentDetail {
+  Id: number;
+  CardOwnerName: string;
+  CardNumber: string;
+  ExpirationDate: string;
+  CVV: string;
+}
---
+++ WebAPI/WebAPI/Models/PaymentDetailContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class PaymentDetailContext : DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> options) : base(options)
+        {
+
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
